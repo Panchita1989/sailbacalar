74c699cf0e223164927901e214284860353a768b
Panchita <franciscalandwehr@hotmail.com>
2025-11-18 09:10:21 -0500

    added google reviews to tours component

---
+++ src/components/reviews.jsx
@@ -0,0 +1,7 @@
+import GoogleReviewsWidget from 'google-reviews-widget'
+
+export default function Reviews(){
+    return(
+        <GoogleReviewsWidget instanceId='qsEnvxlauJ9SMktPIKA2' />
+    )
+}
