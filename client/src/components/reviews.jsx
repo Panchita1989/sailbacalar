89f7fe75107231db22f4b44bc7b5b80bfde917d0
Panchita <franciscalandwehr@hotmail.com>
2025-12-04 13:51:30 -0500

    removed client folder from backend branch

--- client/src/components/reviews.jsx
+++
@@ -1,7 +0,0 @@
-import GoogleReviewsWidget from 'google-reviews-widget'
-
-export default function Reviews(){
-    return(
-        <GoogleReviewsWidget instanceId='qsEnvxlauJ9SMktPIKA2' />
-    )
-}
