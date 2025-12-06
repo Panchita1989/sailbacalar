b84d3731b9af1cdd554b53106693d13f0cabbc15
Panchita <franciscalandwehr@hotmail.com>
2025-12-06 11:30:56 -0500

    updated frontend components

--- client/src/components/banner.jsx
+++ client/src/components/banner.jsx
@@ -17,10 +17,10 @@ export default function Banner(){
         <main className='banner text-teal-950 h-screen flex flex-col md:justify-start justify-center items-center gap-1'>
             <title>Sail Bacalar – Discover Bacalar Lagoon on a Catamaran</title>
             <img className='md:max-h-100  max-h-30 z-10 h1-animate' src="/images/logo.png" alt="sail bacalar logo" />
-            <h1 className='h1 md:text-5xl text-lg banner-content z-10 text-center'>SAIL - ENJOY - GET INSPIRED</h1>
+            <h1 className='h1 md:text-5xl text-lg banner-content z-10 '>SAIL - ENJOY - GET INSPIRED</h1>
             <meta name="description" content="Relax at Pucté Hotel, directly on Bacalar Lagoon. Comfortable rooms, stunning views, and unforgettable boat tours." />
             <meta name="keywords" content="Hotel Bacalar, Bacalar Lagoon, Pucté Hotel, Lagoonfront Hotel, Bacalar Accommodation" />
-            <p className='banner-content md:text-2xl md:mt-25 text-gr content-animate text-center'>DISCOVER BACALAR LAGOON IN OUR 30FT CATAMARAN</p>     
+            <p className='banner-content md:text-2xl text-gr content-animate text-center'>DISCOVER BACALAR LAGOON IN OUR 30FT CATAMARAN</p>     
             <ScrollToButton handleShowMore={handleShowMore} />
         </main>
         </>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,20 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+async function defereRender() {
+  if(process.env.NODE_ENV !== 'development'){
+    return
+  }
+  const { worker } = await import('./mock/browser.js')
+  return worker.start()
+}
+
+defereRender().then(() => {
+  createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <App />
     </BrowserRouter>
   </StrictMode>,
 )
+})
