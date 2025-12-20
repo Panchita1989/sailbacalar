b3381ed311f6fa543770dca8107c40dba4debacc
Panchita <franciscalandwehr@hotmail.com>
2025-12-20 11:23:20 -0500

    added load lazy to gallery

--- client/src/data/gallery.js
+++ client/src/data/gallery.js
@@ -3,17 +3,21 @@
 export const fotoList =[{
     id: 1,
     url: '/images/gallery/pakal.jpg',
-    alt: 'sailboat pakal'
+    alt: 'sailboat pakal',
+    load: 'lazy'
 },{
     id: 2,
     url: '/images/gallery/pakal1.jpg',
-    alt: 'velero'
+    alt: 'velero',
+    load: 'lazy'
 },{
     id: 3,
     url: '/images/gallery/pakalito.jpg',
-    alt: 'sail bacalar'
+    alt: 'sail bacalar',
+    load: 'lazy'
 },{
     id: 4,
     url:'/images/gallery/sail.png',
-    alt:'sail bacalar'
+    alt:'sail bacalar',
+    load: 'lazy'
 }]
