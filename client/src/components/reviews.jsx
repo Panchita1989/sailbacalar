70e99c38cd3c93efd67547acf809e63835a95253
Panchita <franciscalandwehr@hotmail.com>
2025-12-20 11:04:31 -0500

    added google reviews

--- client/src/components/reviews.jsx
+++ client/src/components/reviews.jsx
@@ -1,7 +1,12 @@
-import GoogleReviewsWidget from 'google-reviews-widget'
+
 
 export default function Reviews(){
     return(
-        <GoogleReviewsWidget instanceId='qsEnvxlauJ9SMktPIKA2' />
+        <section>
+            <script src="https://elfsightcdn.com/platform.js" async></script>
+            <div class="elfsight-app-908dbe0b-ddb2-4983-9398-2b4ae639e653" data-elfsight-app-lazy></div>
+        </section>
+
+
     )
 }
