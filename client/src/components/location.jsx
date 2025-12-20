40bb33510a5aa3280a92c43994ff70ee78dab657
Panchita <franciscalandwehr@hotmail.com>
2025-12-20 12:03:41 -0500

    changed embeded map to english

--- client/src/components/location.jsx
+++ client/src/components/location.jsx
@@ -12,7 +12,7 @@ export default function Location(){
             titleAboveImage 
             showButton={false}
             iframe={
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d794.4573740069393!2d-88.38545581017299!3d18.704454933566815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f5bb1c1235ad469%3A0xc202677992c97a4b!2sSail%20bacalar!5e0!3m2!1sde!2smx!4v1763398779961!5m2!1sde!2smx" 
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d794.4573740069393!2d-88.38545581017299!3d18.704454933566815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f5bb1c1235ad469%3A0xc202677992c97a4b!2sSail%20bacalar!5e0!3m2!1sde!2smx!4v1763398779961!5m2!1sen!2smx" 
                 width="800" 
                 height="600" 
                 className="w-full h-80 md:h-100 rounded"
