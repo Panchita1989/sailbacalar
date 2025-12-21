3ff2876c79c58f290cfb2e680d66ea7e3ba55e1f
Panchita <franciscalandwehr@hotmail.com>
2025-12-21 14:53:31 -0500

    fixed button in banner

--- client/src/components/scrollToButton.jsx
+++ client/src/components/scrollToButton.jsx
@@ -12,7 +12,7 @@ export default function ScrollToButton({handleShowMore}){
         <section className='info content-animate  md:text-2xl text-gr text-center '>
             <p>Sail with us</p>
             <Button 
-                content={<FontAwesomeIcon icon={faArrowDown} /> }
+                children={<FontAwesomeIcon icon={faArrowDown} /> }
                 className='p-2 rounded-full bg-neutral-300 text-neutral-800 active:bg-neutral-800 active:text-neutral-300 xl:hover:bg-neutral-800 xl:hover:text-neutral-300'
                 onClick={handleShowMore}/>
         </section>
