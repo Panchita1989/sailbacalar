992bc3e31b764e544504793f1eb5f85a590436bf
Panchita <franciscalandwehr@hotmail.com>
2026-01-08 09:44:41 -0500

    fixed error in english title romantig tour

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,31 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import bookingRoutes from './routes/bookings.js'
+
+dotenv.config()
 
 const app = express()
 
-app.get('/test', (req, res) => {
-  res.send('Server läuft')
-})
+const PORT = process.env.PORT || 5000
 
-app.listen(5000, () => {
-  console.log('Server läuft auf Port 5000')
-})
+
+//Middleware
+app.use(express.json())
+
+//MongoDB connection
+mongoose.connect(process.env.DATABASE_URL)
+  .then(() => console.log('Connected to Database'))
+  .catch(err => console.error(err))
+
+
+//Routes
+app.use('/bookings', bookingRoutes )
 
 app.get('/', (req, res) => {
-  res.send('Hello node js')
+  res.send('Hello Node.js')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server läuft auf Port ${PORT}`)
 })
