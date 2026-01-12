80835fa2f58d6c9a89d3d4b32761deaea1c23e0f
Panchita <franciscalandwehr@hotmail.com>
2026-01-12 12:23:34 -0500

    changed cors function back

--- server/src/server.js
+++ server/src/server.js
@@ -23,14 +23,11 @@ app.use(express.json())
 app.use(cors({ 
   origin: function(origin, callback) {
     if(!origin) return callback(null, true)
-
-    if(allowedOrigins.includes(origin)){
-      return callback(null, true)
-    }else{
+    if(allowedOrigins.indexOf(origin) === -1){
       const msg =  `The CORS policy for this site does not allow access from the specified Origin.`;
       return callback(new Error(msg), false)
     }
-    
+    return callback(null, true)
   } }))
 
 //MongoDB connection
